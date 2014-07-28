54f045ca8747efdd68dd38ce309b907fa6fdf64c
rainu <rainu@raysha.de>
2014-07-28 20:59:36 +0200

    add test for makro feature

---
+++ core/src/test/java/de/raysha/lib/jsimpleshell/MakroTest.java
@@ -0,0 +1,42 @@
+package de.raysha.lib.jsimpleshell;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.raysha.lib.jsimpleshell.exception.CLIException;
+
+public class MakroTest {
+
+	SilentShell shell;
+	
+	@Before
+	public void setup() throws IOException{
+		ShellBuilder builder = ShellBuilder.shell("jss");
+		
+		shell = new SilentShell(builder);
+	}
+	
+	@Test
+	public void recordMakro() throws IOException, CLIException{
+		File makro = File.createTempFile("jsimpleshell", ".makro");
+		makro.deleteOnExit();
+		
+		shell.executeCommand("start-record", makro.getAbsolutePath());
+		shell.executeCommand("?list");
+		shell.executeCommand("?list-all");
+		shell.executeCommand("unknownCommand");
+		shell.executeCommand("stop-record");
+		
+		String makroContent = FileUtils.readFileToString(makro);
+		String expectedContent = "?list\n?list-all\nunknownCommand\n";
+		
+		assertTrue("Makro wasn't created correctely! " + makroContent,
+				makroContent.endsWith(expectedContent));
+	}
+}
