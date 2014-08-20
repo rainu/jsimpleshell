ca88ae7070b92cfbaece92977ed783b1fa23790f
rainu <rainu@raysha.de>
2014-08-20 19:05:48 +0200

    add integrationtest for parametrized scripts

--- integrationstests/src/test/java/de/raysha/lib/jsimpleshell/it/basic/Script.java
+++ integrationstests/src/test/java/de/raysha/lib/jsimpleshell/it/basic/Script.java
@@ -33,4 +33,29 @@ public class Script extends IntegrationsTest {
 		assertTrue(result.containsOutLine("IT\\> \\?help \\?list"));
 		assertFalse(result.containsOutLine("this is a comment line"));
 	}
+	
+	@Test
+	public void runParametrizedScript() throws IOException{
+		File tmpScript = File.createTempFile("jss", ".script");
+		tmpScript.deleteOnExit();
+		
+		FileUtils.write(tmpScript, "echo {value}\n", true);
+		FileUtils.write(tmpScript, "?help\n", true);
+		FileUtils.write(tmpScript, "#this is a comment line\n", true);
+		FileUtils.write(tmpScript, "\n", true);
+		FileUtils.write(tmpScript, "?help ?list\n", true);
+		FileUtils.write(tmpScript, "echo {value}\n", true);
+		
+		executeAndWaitForCommand("?help", "exit");
+		CommandResult result = executeAndWaitForCommand("!run-script", 
+				tmpScript.getAbsolutePath(), "value=Hello World");
+		
+		assertFalse(result.isError());
+
+		assertTrue(result.containsOutLine("IT\\> echo Hello World"));
+		assertFalse(result.containsOutLine("IT\\> echo {value}"));
+		assertTrue(result.containsOutLine("IT\\> \\?help ?list"));
+		assertTrue(result.containsOutLine("IT\\> \\?help \\?list"));
+		assertFalse(result.containsOutLine("this is a comment line"));
+	}
 }
