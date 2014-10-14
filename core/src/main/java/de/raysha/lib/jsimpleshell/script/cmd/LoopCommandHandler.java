702109107f0078d0baaa0d15028d46b8ff750d77
rainu <rainu@raysha.de>
2014-10-14 18:18:34 +0200

    finalise counting-loop

--- core/src/main/java/de/raysha/lib/jsimpleshell/script/cmd/LoopCommandHandler.java
+++ core/src/main/java/de/raysha/lib/jsimpleshell/script/cmd/LoopCommandHandler.java
@@ -11,10 +11,12 @@ import de.raysha.lib.jsimpleshell.Shell;
 import de.raysha.lib.jsimpleshell.ShellCommand;
 import de.raysha.lib.jsimpleshell.annotation.Command;
 import de.raysha.lib.jsimpleshell.annotation.Inject;
+import de.raysha.lib.jsimpleshell.annotation.Param;
 import de.raysha.lib.jsimpleshell.builder.ShellBuilder;
 import de.raysha.lib.jsimpleshell.exception.CLIException;
 import de.raysha.lib.jsimpleshell.exception.ExitException;
 import de.raysha.lib.jsimpleshell.handler.MessageResolver;
+import de.raysha.lib.jsimpleshell.script.Environment;
 import de.raysha.lib.jsimpleshell.util.MultiMap;
 
 /**
@@ -23,14 +25,39 @@ import de.raysha.lib.jsimpleshell.util.MultiMap;
  */
 public class LoopCommandHandler {
 
+	public static final String VARIABLE_NAME_FROM = "_LF";	//LOOP_FROM
+	public static final String VARIABLE_NAME_UNTIL = "_LU"; //LOOP_UNTIL
+	public static final String VARIABLE_NAME_STEP = "_LS"; //LOOP_STEP
+	public static final String VARIABLE_NAME_COUNT = "_LC"; //LOOP_COUNT
+
 	@Inject
 	private Shell shell;
 
 	@Inject
 	private MessageResolver messageResolver;
 
-	@Command(startsSubshell = true)
-	public void loop() throws IOException, CLIException{
+	@Inject
+	private Environment environment;
+
+	@Command(abbrev = "command.abbrev.loop.count", description = "command.description.loop.count",
+			header = "command.header.loop.count", name = "command.name.loop.count", startsSubshell = true)
+	public void loopCount(
+			@Param(value = "param.name.loop.count.1", description = "param.description.loop.count.1")
+			Long until) throws IOException, CLIException{
+
+		loopCount(0L, until, 1L);
+	}
+
+	@Command(abbrev = "command.abbrev.loop.count", description = "command.description.loop.count",
+			header = "command.header.loop.count", name = "command.name.loop.count", startsSubshell = true)
+	public void loopCount(
+			@Param(value = "param.name.loop.count", description = "param.description.loop.count")
+			Long from,
+			@Param(value = "param.name.loop.count.1", description = "param.description.loop.count.1")
+			Long until,
+			@Param(value = "param.name.loop.count.2", description = "param.description.loop.count.2")
+			Long step) throws IOException, CLIException{
+
 		final LoopPrompt myPrompt = new LoopPrompt();
 
 		Shell subshell = ShellBuilder.subshell(myPrompt, shell)
@@ -49,12 +76,54 @@ public class LoopCommandHandler {
 			List<String> commands = recorder.recordCommands();
 			purge(commands);
 
-			shell.getPipeline().prepend(commands);
-			shell.getPipeline().prepend(commands);
+			for(long i=from; i < until; i += step){
+				setEnvironmentVariables(i, from, until, step);
+				shell.getPipeline().prepend(commands);
+			}
+			setRemoveVariables();
 		}else{
 			//i am a sub-loop
 			subshell.commandLoop();
 		}
+	}
+
+	private void setRemoveVariables() {
+		shell.getPipeline().prepend(
+				removeGlobalVariable(VARIABLE_NAME_COUNT),
+				removeGlobalVariable(VARIABLE_NAME_FROM),
+				removeGlobalVariable(VARIABLE_NAME_UNTIL),
+				removeGlobalVariable(VARIABLE_NAME_STEP));
+	}
+
+	private String removeGlobalVariable(String name) {
+		//TODO: implement remove variables!
+		return "";
+	}
+
+	private void setEnvironmentVariables(Long offset, Long from, Long until, Long step) {
+		shell.getPipeline().prepend(
+				setGlobalVariable(VARIABLE_NAME_COUNT, offset),
+				setGlobalVariable(VARIABLE_NAME_FROM, from),
+				setGlobalVariable(VARIABLE_NAME_UNTIL, until),
+				setGlobalVariable(VARIABLE_NAME_STEP, step));
+	}
+
+	private String setGlobalVariable(String name, Object value) {
+		final String command = "." + messageResolver.resolveGeneralMessage(EnvironmentCommandHandler.COMMAND_NAME_SET_GLOBAL_VARIABLE);
+		final String varName = name + getLoopCount();
+
+		return command + " \"" + varName + "\" \"" + String.valueOf(value) + "\"";
+	}
+
+	private Integer getLoopCount() {
+		int count = 0;
+		for(PromptElement part : shell.getPath()){
+			if(part instanceof LoopPrompt){
+				count++;
+			}
+		}
+
+		return count;
 	}
 
 	private void purge(List<String> commands) {
@@ -81,12 +150,20 @@ public class LoopCommandHandler {
 	}
 
 	private void copyMainHandler(Shell subshell) {
-		List<Object> mainHandler = getMainHandler();
-		for(Object handler : mainHandler){
+		List<Object> mainHandler = getMainHandler(shell);
+		List<Object> defaultHandler = getMainHandler(ShellBuilder.shell("").build());
+
+		mainLoop: for(Object handler : mainHandler){
 			if(	handler instanceof LoopCommandHandler ||
 				handler instanceof LoopSpecialCommand){
 
 				continue;
+			}
+			for(Object defHandler : defaultHandler){
+				if(handler.getClass() == defHandler.getClass()){
+					//the current handler is an default handler!
+					continue mainLoop;
+				}
 			}
 
 			for(ShellCommand cmd : shell.getCommandTable().getCommandTable()){
@@ -98,7 +175,7 @@ public class LoopCommandHandler {
 		}
 	}
 
-	private List<Object> getMainHandler() {
+	private List<Object> getMainHandler(Shell shell) {
 		List<Object> aux = getAuxHandler();
 		List<Object> all = shell.getAllHandler();
 
@@ -122,12 +199,14 @@ public class LoopCommandHandler {
 	public class LoopPrompt implements PromptElement {
 		@Override
 		public String render() {
-			return "loop";
+			return messageResolver.resolveGeneralMessage("message.loop.prompt");
 		}
 	}
 
 	public class LoopSpecialCommand {
-		@Command
+
+		@Command(abbrev = "command.abbrev.loop.end", description = "command.description.loop.end",
+				header = "command.header.loop.end", name = "command.name.loop.end")
 		public void loopEnd() throws ExitException {
 			throw new ExitException();
 		}
