daf3c3f8ac481ea9ca0d720d792a3b5a8d395c3e
rainu <rainu@raysha.de>
2015-02-10 21:32:13 +0100

    add tests for the Issue: Autocomplete with candidates that includes whitespaces

--- integrationstests/src/main/java/de/raysha/lib/jsimpleshell/CompleterCommands.java
+++ integrationstests/src/main/java/de/raysha/lib/jsimpleshell/CompleterCommands.java
@@ -1,7 +1,10 @@
 package de.raysha.lib.jsimpleshell;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import de.raysha.lib.jsimpleshell.annotation.Command;
 import de.raysha.lib.jsimpleshell.annotation.Param;
@@ -42,10 +45,27 @@ public class CompleterCommands implements CandidatesChooser {
 		return "custom: " + customType;
 	}
 
+	@Command
+	public String setText(
+			@Param(value = "p", type = "Text")
+			String text){
+		return "text: " + text;
+	}
+
 	@Override
 	public Candidates chooseCandidates(ShellCommandParamSpec paramSpec, String part) {
 		if("CompleterCommands".equals(paramSpec.getType())){
 			return new Candidates(Arrays.asList("Candidate1", "Candidate2"));
+		}else if("Text".equals(paramSpec.getType())){
+			List<String> candidates = new ArrayList<String>(Arrays.asList("ABC Test One", "ABC Test Two", "ABC 2015"));
+			Iterator<String> iter = candidates.iterator();
+			while(iter.hasNext()){
+				if(!iter.next().startsWith(part)){
+					iter.remove();
+				}
+			}
+
+			return new Candidates(candidates);
 		}
 		return null;
 	}
