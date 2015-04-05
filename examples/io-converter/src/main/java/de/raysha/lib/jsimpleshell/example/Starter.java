f4e72d84facd042e48950a060ce79734aeac2a32
rainu <rainu@raysha.de>
2015-04-05 15:14:21 +0200

    describe the input-/output converter

--- examples/io-converter/src/main/java/de/raysha/lib/jsimpleshell/example/Starter.java
+++ examples/io-converter/src/main/java/de/raysha/lib/jsimpleshell/example/Starter.java
@@ -11,6 +11,9 @@ public class Starter {
 	public static void main(String[] args) throws IOException, CLIException {
 		final Shell shell = ShellBuilder.shell("JSS")
 								.behavior()
+									.addAuxHandler(new DateInputConverter())	//register the input converter
+									.addAuxHandler(new DateOutputConverter())	//register the output converter
+									.addHandler(new Commands())
 								.build();
 
 		//print help text
