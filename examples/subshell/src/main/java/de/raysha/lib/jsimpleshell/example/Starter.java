2af08b3e86b20a94a6027059749e0e45fd503325
rainu <rainu@raysha.de>
2015-09-05 14:02:56 +0200

    add command as handler

--- examples/subshell/src/main/java/de/raysha/lib/jsimpleshell/example/Starter.java
+++ examples/subshell/src/main/java/de/raysha/lib/jsimpleshell/example/Starter.java
@@ -1,16 +1,17 @@
 package de.raysha.lib.jsimpleshell.example;
 
-import java.io.File;
-import java.io.IOException;
-
 import de.raysha.lib.jsimpleshell.Shell;
 import de.raysha.lib.jsimpleshell.builder.ShellBuilder;
 import de.raysha.lib.jsimpleshell.exception.CLIException;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Starter {
 	public static void main(String[] args) throws IOException, CLIException {
 		final Shell shell = ShellBuilder.shell("JSS")
 								.behavior()
+									.addHandler(new Commands())
 								.build();
 
 		//print help text
